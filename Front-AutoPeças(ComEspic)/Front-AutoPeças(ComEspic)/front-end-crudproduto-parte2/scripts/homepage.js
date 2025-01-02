f78ce4118ff25851981137feb9017d5e2c16da0a
Quingu <132612304+Quingu@users.noreply.github.com>
2025-01-02 15:20:40 -0300

    Add files via upload

---
+++ Front-AutoPeças(ComEspic)/Front-AutoPeças(ComEspic)/front-end-crudproduto-parte2/scripts/homepage.js
@@ -0,0 +1,8 @@
+document.getElementById("list-button").onclick = function () {
+    window.location.href = "listallproducts.html";
+};
+
+
+document.getElementById("new-product-button").onclick = function () {
+    window.location.href = "addproduct.html";
+};
