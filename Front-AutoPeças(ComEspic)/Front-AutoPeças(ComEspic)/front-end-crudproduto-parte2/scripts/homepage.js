5755ff7999055706ab031a3576a27766cb20e460
Quingu <132612304+Quingu@users.noreply.github.com>
2025-01-02 15:59:59 -0300

    Delete Front-AutoPeças(ComEspic)/Front-AutoPeças(ComEspic)/front-end-crudproduto-parte2 directory

--- Front-AutoPeças(ComEspic)/Front-AutoPeças(ComEspic)/front-end-crudproduto-parte2/scripts/homepage.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("list-button").onclick = function () {
-    window.location.href = "listallproducts.html";
-};
-
-
-document.getElementById("new-product-button").onclick = function () {
-    window.location.href = "addproduct.html";
-};
